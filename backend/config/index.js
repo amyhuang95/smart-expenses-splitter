f9f09a31ddbd458319365a1ee612aef2a9e14a51
Amy Huang <62624198+amyhuang95@users.noreply.github.com>
2026-03-17 12:26:16 -0700

    feat: add check for session secret

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,5 +1,9 @@
 import "dotenv/config";
 
+if (!process.env.SESSION_SECRET) {
+  throw new Error("SESSION_SECRET is not set in environment variables.");
+}
+
 export const config = {
   port: process.env.PORT || 3000,
   mongoUri: process.env.MONGODB_URI,
